acaf6ad0ff254c7e35a35c6201cee572172c34ea
DeveloperSAJ <developersaj981@gmail.com>
2025-12-15 22:02:25 +0500

    Initial Commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Profiile() {
+  return (
+    <View>
+      <Text>Profiile</Text>
+    </View>
+  )
+}
---
+++ styles/auth.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title:{
+    color: "lightblue",
+    fontSize: 50,
+  }
+})
+
+export default styles;
