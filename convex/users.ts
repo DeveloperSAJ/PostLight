51cb963fc505d7184de1ebc645db50131573d7b0
DeveloperSAJ <developersaj981@gmail.com>
2025-12-18 20:10:16 +0500

    create screen added

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { mutation } from "./_generated/server";
+import { mutation, MutationCtx, QueryCtx } from "./_generated/server";
 
 export const createUser = mutation({
     args:{
@@ -15,9 +15,11 @@ export const createUser = mutation({
         const existingUser = await ctx.db.query("users")
         .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
         .first()
-
+        
         if(existingUser) return;
 
+
+        //create a user in db
         await ctx.db.insert("users", {
             username: args.username,
             fullname: args.fullname,
@@ -31,3 +33,14 @@ export const createUser = mutation({
         })
     }
 });
+
+export async function getAuthenticatedUser(ctx:QueryCtx | MutationCtx) {
+    const identify = await ctx.auth.getUserIdentity();
+    if (!identify) throw new Error("Unauthorized");
+    const currentUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", identify.subject))
+      .first();
+
+    if (!currentUser) throw new Error("User not found");
+}
